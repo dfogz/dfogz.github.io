52afefb00194b012d4d41201861d539aa32709ac
Dylan Fogarty <dyfogarty@gmail.com>
2018-12-28 15:33:14 -0500

    Added CTA button

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 $(document).ready(function(){
 
-  $('div#about').hide();
-  $('div#travels').hide();
+  $('#about').hide();
+  $('#reads').hide();
+  $('#travels').hide();
+  $('#social').hide();
 
   $('#link_about').click(function(){ 
     $('#quotes').hide();
-    $('#travels').hide();
     $('#about').show();
+    $('#reads').hide();
+    $('#travels').hide();
+    $('#social').hide();
   });
 
-  $('#link_quotes').click(function(){ 
+  $('#link_reads').click(function(){ 
+    $('#quotes').hide();
     $('#about').hide();
+    $('#reads').show();
     $('#travels').hide();
-    $('#quotes').show();
-  }); 
+    $('#social').hide();
+  });
   
   $('#link_travels').click(function(){ 
     $('#quotes').hide();
     $('#about').hide();
+    $('#reads').hide();
     $('#travels').show();
+    $('#social').hide();
+  });
+  
+  $('#link_social').click(function(){ 
+    $('#quotes').hide();
+    $('#about').hide();
+    $('#reads').hide();
+    $('#travels').hide();
+    $('#social').show();
   });
 });
